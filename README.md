9abfa8f013f733e5518d27cc2380e912fed0c6ba
FleitasEzequiel <ezequielafeita@gmail.com>
2025-04-07 12:59:17 -0300

    Tarea

---
+++ README.md
@@ -0,0 +1,20 @@
+# Instrucciones de Ejecución
+## Prender servidor
+Navega a la carpeta del servidor:
+
+```bash
+cd server
+```
+Instalar las dependencias:
+
+```bash
+npm install
+```
+
+Encender el servidor :
+```bash
+node server.js
+```
+
+## Página Web
+ Abrir el archivo index.html , no es tan difícil
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require("cors")
+const port = 3000;
+app.use(cors({
+    origin:"null"
+}))
+app.get('/saludo/:nombre', (req, res) => {
+const nombre = req.params.nombre;
+res.send(`Hola, ${nombre}!`);
+});
+app.listen(port, () => {
+console.log(`Servidor escuchando en el puerto ${port}`);
+});
